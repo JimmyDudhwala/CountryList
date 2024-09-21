ce99258ffe03e83e7a973cc2015bc041f29d2a90
Jimmy <jimmydudhwala25@gmail.com>
2024-09-22 02:42:17 +0530

    publish'

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ep8/',
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-})
+});
